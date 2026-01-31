c98082845324cc217b1ac5d63c56ef470dcbce52
ShashwatSricodes <shashwatwrites@gmail.com>
2026-01-31 10:17:04 +0530

    FInal

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -40,13 +40,19 @@ const PaperTexture = () => (
 async function createSupabaseServer() {
   const cookieStore = await cookies();
 
+  interface CookieSetOptions {
+    name: string;
+    value: string;
+    options?: Record<string, unknown>;
+  }
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll: () => cookieStore.getAll(),
-        setAll: (cookiesToSet) => {
+        setAll: (cookiesToSet: CookieSetOptions[]) => {
           cookiesToSet.forEach(({ name, value, options }) => {
             cookieStore.set(name, value, options);
           });
